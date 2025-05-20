4f9f348583b2bc10350cbd244a1d9117a0795de7
juzdalua <bearwcw@naver.com>
2025-05-20 13:59:48 +0900

    First Commit

---
+++ src/main/java/com/notepad/notepadjavavsc/NotepadJavaVscApplication.java
@@ -0,0 +1,13 @@
+package com.notepad.notepadjavavsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotepadJavaVscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotepadJavaVscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notepad/notepadjavavsc/NotepadJavaVscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notepad.notepadjavavsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadJavaVscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
