fc99140b19aa089c987fa7f6ae00baeee407e64f
juzdalua <bearwcw@naver.com>
2025-05-20 17:48:00 +0900

    Feat: Add global http exception filter

--- src/main/java/com/notepad/notepadjavavsc/config/GlobalExceptionHandler.java
+++ src/main/java/com/notepad/notepadjavavsc/config/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package com.notepad.notepadjavavsc.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +18,13 @@ public class GlobalExceptionHandler {
     error.put("success", false);
     error.put("message", ex.getMessage());
     return ResponseEntity.badRequest().body(error);
+  }
+
+  @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+  public ResponseEntity<Map<String, Object>> handleMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+    Map<String, Object> error = new HashMap<>();
+    error.put("success", false);
+    error.put("message", "Invalid URL");
+    return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(error); // 405
   }
 }
