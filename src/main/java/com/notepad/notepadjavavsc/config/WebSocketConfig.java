45046260d16c88e82d6179f4936ac8bd3cb27425
juzdalua <bearwcw@naver.com>
2025-05-26 11:13:55 +0900

    Feat: Add WS

---
+++ src/main/java/com/notepad/notepadjavavsc/config/WebSocketConfig.java
@@ -0,0 +1,41 @@
+package com.notepad.notepadjavavsc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.notepad.notepadjavavsc.common.dto.MessagePayloadDto;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(simpleWebSocketHandler(), "/").setAllowedOrigins("*");
+  }
+
+  @Bean
+  public WebSocketHandler simpleWebSocketHandler() {
+    return new TextWebSocketHandler() {
+      @Override
+      protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        System.out.println("MessageStr: " + payload);
+
+        MessagePayloadDto jsonMessage = objectMapper.readValue(payload, MessagePayloadDto.class);
+
+        session.sendMessage(new TextMessage("Echo: " + payload));
+      }
+    };
+  }
+}
