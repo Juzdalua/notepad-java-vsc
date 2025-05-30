b1e9f608de1d4754dee126cca8f1a49b57155d8f
juzdalua <bearwcw@naver.com>
2025-05-30 20:02:42 +0900

    Fix: DB
    - Update REMEAD for DB create table
    - Update DB-user Entity

--- src/main/java/com/notepad/notepadjavavsc/user/domain/User.java
+++ src/main/java/com/notepad/notepadjavavsc/user/domain/User.java
@@ -7,8 +7,21 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
--- src/main/java/com/notepad/notepadjavavsc/user/repository/UserRepositoryCustom.java
+++ src/main/java/com/notepad/notepadjavavsc/user/repository/UserRepositoryCustom.java
@@ -1,5 +1,5 @@
 package com.notepad.notepadjavavsc.user.repository;
 
 public interface UserRepositoryCustom {
-   long updateUserAgeByNameFromQueryDsl(String name, int age);
+   long updateUserAgeByEmailFromQueryDsl(String email);
 }
